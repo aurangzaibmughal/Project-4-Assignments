60733eacf1f046fb9651480dea996166f627036a
aurangzaibmughal <162279456+aurangzaibmughal@users.noreply.github.com>
2025-03-26 00:53:00 +0500

    Add files via upload
    canvas-eraser completed

---
+++ main.py
@@ -0,0 +1,64 @@
+
+from cmath import rect
+from hmac import new
+import time
+from turtle import Screen
+from xml.etree.ElementTree import PI
+import pygame
+
+pygame.init()
+    
+CANVAS_WIDTH : int = 400
+CANVAS_HEIGHT : int = 400
+
+CELL_SIZE : int = 40
+ERASER_SIZE : int = 20
+
+BLUE : tuple = (0, 0, 255)
+WHITE : tuple = (255, 255, 255)
+PINK : tuple = (255, 105, 180)
+
+screen = pygame.display.set_mode((CANVAS_WIDTH, CANVAS_HEIGHT))
+pygame.display.set_caption("Enter effect in pygame")
+
+grid = []
+for row in range(0,CANVAS_HEIGHT , CELL_SIZE):
+    for col in range(0, CANVAS_WIDTH, CELL_SIZE):
+        rect = pygame.Rect(col, row, CELL_SIZE, CELL_SIZE)
+        grid.append(rect)
+eraser = pygame.Rect(200, 200, ERASER_SIZE, ERASER_SIZE) 
+
+runing = True
+while runing:
+    screen.fill(WHITE)
+    for rect in grid:
+        pygame.draw.rect(screen, BLUE, rect)
+ 
+    mouse_x, mouse_y = pygame.mouse.get_pos()
+    eraser.topleft = (mouse_x, mouse_y)
+
+    new_grid = []
+    for rect in grid:
+        if not eraser.colliderect(rect):
+            new_grid.append(rect)
+    grid = new_grid
+
+    pygame.draw.rect(screen, PINK, eraser)
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            runing = False
+    pygame.display.flip()
+    time.sleep(0.05)
+pygame.quit()                
+
+   
+
+
+
+
+
+    
+    
+    
+    
